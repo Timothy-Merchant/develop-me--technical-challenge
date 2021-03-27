4d5d81051127d9e98606dcb0b2d5e81c548527c8
Tim <ryalin1286@gmail.com>
2021-03-27 09:47:25 +0000

    Finalized Pairing component to MVP standard, with dummy text for player names, scores and win conditions. Finalized global font style and imported from Google fonts, then added to Global sass file as a variable ''.

--- src/components/Pairing/Pairing.js
+++ src/components/Pairing/Pairing.js
@@ -2,10 +2,20 @@ import '../../styles/Pairing.scss';
 
 const Pairing = () => {
     return (
-        <div className="pairing__wrapper">
-            <div className="pairing__player"></div>
-            <div className="pairing__player"></div>
-        </div>
+        <table>
+            <tbody>                
+                <tr className="pairing__player">
+                    <td className="pairing__name">Player1</td>
+                    <td className="pairing__score">21</td>
+                    <td className="pairing__win">LOST</td>
+                </tr>
+                <tr className="pairing__player">
+                    <td className="pairing__name">Player2</td>
+                    <td className="pairing__score">23</td>
+                    <td className="pairing__win">WON</td>
+                </tr>
+            </tbody>
+        </table>
     );
 }
 
