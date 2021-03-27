0548864392080326b75e0f8061319df83c8858c5
Tim <ryalin1286@gmail.com>
2021-03-27 11:47:20 +0000

    Successfully styled the TournamentTree component to have a tree-like tournament bracket style using CSS grid to stagger the heights of the respective PairingRow components.

--- src/components/TournamentTree/TournamentTree.js
+++ src/components/TournamentTree/TournamentTree.js
@@ -8,9 +8,15 @@ const TournamentTree = () => {
         <div className="TournamentTree__Left">
             {/* The number of Pairings will vary based on amount of players decided by user.*/}
             {/* Will support 2, 4, 6, 8 & 10. */}
-            <PairingRow matches={["match", "match", "match", "match"]} title={"Quarter Finals"} />
-            <PairingRow matches={["match", "match"]} title={"Semi Finals"} />
-            <PairingRow matches={["match"]} title={"Finals"} />
+            <div className="TournamentTree__QuarterFinals">
+                <PairingRow matches={["match", "match", "match", "match"]} title={"Quarter Finals"} />
+            </div>
+            <div className="TournamentTree__SemiFinals">
+                <PairingRow matches={["match", "match"]} title={"Semi Finals"} />
+            </div>
+            <div className="TournamentTree__Finals">
+                <PairingRow matches={["match"]} title={"Finals"} />
+            </div>
         </div>
     );
 }
