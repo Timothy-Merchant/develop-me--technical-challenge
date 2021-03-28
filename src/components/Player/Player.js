f4afd89f95131f4d9543a5d126b321ed1e9cd73e
Tim <ryalin1286@gmail.com>
2021-03-28 11:10:22 +0100

    Minor update of the Player component to no longer include Service information as that is now handled in the parent MatchBox component.

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -5,8 +5,7 @@ const Player = () => {
         <div className="Player__Wrapper">
             <h1>Player 1</h1>
             <p>Score</p>
-            <p className="Player__Score">21</p>
-            <p className="Player__Service">Service</p>
+            <p className="Player__Score">21</p>            
             <button className="Player__Button">+</button>
         </div>
     );
