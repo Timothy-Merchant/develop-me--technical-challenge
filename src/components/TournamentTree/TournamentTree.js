205f144e92bfd3907a3ba78f8cdf0bfb037bb59f
Tim <ryalin1286@gmail.com>
2021-04-06 14:56:36 +0100

    Created a simple function as part of the TournamentTree component to call rounds as Final, Semi Final or Quarter Final depending on their position, and if the game is large enough to warrant further rounds, then the Round + number of the round.

--- src/components/TournamentTree/TournamentTree.js
+++ src/components/TournamentTree/TournamentTree.js
@@ -3,13 +3,19 @@ import Pairing from '../Pairing';
 
 const TournamentTree = ({ gameStarted, games, rounds }) => {
 
+    const roundNaming = (index, length) => (
+        index === length ? "Final" :
+            index === length - 1 ? "Semi Finals" :
+                index === length - 2 ? "Quarter Finals" : `Round ${index}`
+    )
+
     return (
         gameStarted ?
             <div className="TournamentTree">
                 {rounds.map((round, index) => (
                     <div key={index} style={{ display: "flex", flexDirection: "column" }}>
                         <div className="PairingRow">
-                            <h1 className="PairingRow__title">Round {round.id}</h1>
+                            <h1 className="PairingRow__title">{roundNaming(index + 1, rounds.length)}</h1>
                             {round.games.map((game, index) => (
                                 <Pairing game={game} key={index} />
                             ))}
